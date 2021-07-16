48e9ff56ad435a947073e8489874974df0253b92
Sukrit Walia <sukritwalia@gmail.com>
2021-07-16 17:45:46 -0400

    Remove file filter from test helper.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,15 +1,12 @@
+require 'simplecov'
+SimpleCov.start 'rails'
+
+require 'codecov'
+SimpleCov.formatter = SimpleCov::Formatter::Codecov
+
 ENV['RAILS_ENV'] ||= 'test'
 require_relative "../config/environment"
 require "rails/test_help"
-require 'simplecov'
-require 'codecov'
-
-SimpleCov.start 'rails' do
-  add_filter '/bin/'
-  add_filter '/db/'
-  add_filter '/test/'
-end
-SimpleCov.formatter = SimpleCov::Formatter::Codecov
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
