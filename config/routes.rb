36d591cc95515677a48ff96bdf6656f2fcba58d2
Sukrit Walia <sukritwalia@gmail.com>
2021-07-16 18:24:43 -0400

    Fix root route.

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   devise_for :users
   resources :projects, only: [:index]
-  root to: 'project#index'
+  root to: 'projects#index'
 end
