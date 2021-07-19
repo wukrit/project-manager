a132e345032e1f5e715ee1705d51ce00b024df46
Sukrit Walia <sukritwalia@gmail.com>
2021-07-19 18:13:06 -0400

    Allow basic project creation.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,4 +2,25 @@ class ProjectsController < ApplicationController
   def index
     @projects = current_user.projects
   end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+
+    if @project.save
+      Membership.create(project: @project, user: current_user)
+      redirect_to projects_path
+    else
+      render action: 'new'
+    end
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:title, :description)
+  end
 end
